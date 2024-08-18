2b4bddf7903c98f45906d882ffd1615e57405e8f
Anees <aneesnafeesimmu@gmail.com>
2024-08-18 07:18:20 +0530

    navbar-edit

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,7 @@ import Projects from "./Projects";
 import Skills from "./Skills";
 const Body = () => {
     return (
-        <div className="">
+        <div className="mt-20">
         <div className="right-0 absolute bg-gradient-to-r from-start-color to-end-color  w-56 h-[900px] z-[-1] blur-[90px] translate-y-[-250px] rotate-[105deg] opacity-30"></div>
             <div className="m-14 flex text-white">
                 <div className="mx-auto my-auto w-8/12">
@@ -14,10 +14,10 @@ const Body = () => {
                         <h1 className="mt-5">As a <span className="text-text-color"> 2024 BTech graduate in Computer Science, </span> I am ready to apply my technical knowledge and problem-solving skills in a challenging role. My education has prepared me for a career that demands innovation and collaboration. I am seeking a position where I can contribute to impactful projects and grow as a software development professional.</h1>
                     </div>
                     <div className="mt-5">
-                        <a className="inline-block text-xl bg-gradient-to-r from-teal-400 to-blue-500 text-white text-center py-1 px-2 rounded-full font-medium hover:scale-110 hover:shadow-[0_0_10px_rgba(255,128,128,0.9)] transition-all duration-300 ease-in-out" href="https://drive.google.com/file/d/15uJA6c_5lLzIMIGIWpyuIEP5R0gxvciW/view" target="_blank">Resume</a>                       
-                        <a className="inline-block text-xl bg-gradient-to-r from-teal-400 to-blue-500 text-white text-center ml-4 py-1 px-2 rounded-full font-medium hover:scale-110 hover:shadow-[0_0_10px_rgba(255,128,128,0.9)] transition-all duration-300 ease-in-out" href="https://github.com/Aneeshaik" target="_blank">GitHub</a>
-                        <a className="inline-block text-xl bg-gradient-to-r from-teal-400 to-blue-500 text-white text-center ml-4 py-1 px-2 rounded-full font-medium hover:scale-110 hover:shadow-[0_0_10px_rgba(255,128,128,0.9)] transition-all duration-300 ease-in-out" href="https://www.linkedin.com/in/shaik-anees-ahmad/" target="_blank">LinkedIn</a>
-                        <p className="mt-5">const linkedIn = <a href="https://www.linkedin.com/in/shaik-anees-ahmad/">https://www.linkedin.com/in/shaik-anees-ahmad/</a></p>
+                        {/* <a className="inline-block text-xl bg-gradient-to-r from-teal-400 to-blue-500 text-white text-center ml-4 py-1 px-2 rounded-full font-medium hover:scale-110 hover:shadow-[0_0_10px_rgba(255,128,128,0.9)] transition-all duration-300 ease-in-out" href="https://github.com/Aneeshaik" target="_blank">GitHub</a>
+                        <a className="inline-block text-xl bg-gradient-to-r from-teal-400 to-blue-500 text-white text-center ml-4 py-1 px-2 rounded-full font-medium hover:scale-110 hover:shadow-[0_0_10px_rgba(255,128,128,0.9)] transition-all duration-300 ease-in-out" href="https://www.linkedin.com/in/shaik-anees-ahmad/" target="_blank">LinkedIn</a> */}
+                        <p className="mt-5">const linkedIn = <a href="https://www.linkedin.com/in/shaik-anees-ahmad/" target="_blank">" <span className="text-text-color">https://www.linkedin.com/in/shaik-anees-ahmad</span>"</a></p>
+                        <p className="mt-5">const gitHub = <a href="https://github.com/Aneeshaik" target="_blank">"<span className="text-text-color">https://github.com/Aneeshaik</span>"</a></p>
                     </div>
                     </div>
                         <div className="relative w-4/12">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 const Header = () => {
+    useEffect(() => {
+        // Setup: Adding the scroll listener
+        const handleScroll = () => {
+          const navbar = document.getElementById('navbar');
+          if (window.scrollY > 50) {
+            navbar.classList.add('backdrop-blur-md', 'bg-black/40');
+          } else {
+            navbar.classList.remove('backdrop-blur-md', 'bg-black/40');
+          }
+        };
+      
+        window.addEventListener('scroll', handleScroll);
+      
+        // Cleanup: Removing the scroll listener when the component is removed
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
     return (
+        <div id="navbar" className="fixed w-full z-10">
         <div className="flex p-4 justify-between text-white">
                 <div>
                     <h1 className="">Shaik Anees Ahmad</h1>
+                </div>
+                <div className="absolute left-1/2 transform -translate-x-1/2">
+                    <a className="inline-block text-xl bg-gradient-to-r from-teal-400 to-blue-500 text-white text-center my-[-4px] py-1 px-2 rounded-full font-medium hover:scale-110 hover:shadow-[0_0_10px_rgba(255,128,128,0.9)] transition-all duration-300 ease-in-out" href="https://drive.google.com/file/d/15uJA6c_5lLzIMIGIWpyuIEP5R0gxvciW/view" target="_blank">Resume</a>                   
                 </div>
                 <div>
                 <ul className="flex">
@@ -13,6 +36,7 @@ const Header = () => {
                     <li className=""><Link to="/contact">Contact me</Link></li>
                     </ul>
                 </div>
+        </div>
         </div>
     )
 }
